3d69e143f35e4fcdc3b0e9c5375a3b8dac2024ca
Anandi Saputra <sptraanandi@gmail.com>
2023-04-01 12:05:18 +0800

    Test Push

--- controllers/Discussion.js
+++ controllers/Discussion.js
@@ -1,4 +1,4 @@
-// import { where } from "sequelize";
+import {Op} from "sequelize";
 import Discussion from "../models/DiscussionModel.js";
 import Users from "../models/UserModel.js";
 
@@ -7,15 +7,21 @@ export const getDiscussion = async (req , res) => {
         let response;
         if(req.role === "admin"){
             response = await Discussion.findAll({
+                // attributes:['uuid','title','content'],
                 include:[{
                     model: Users, 
-                    // attributes: ['name','email']
+                    // attributes: ['title','content']
                 }]
             });
         }else{
             response = await Discussion.findAll({
+                // attributes:['uuid','title','content'],
+                where:{
+                    userId: req.userId
+                },
                 include:[{
-                    model: Users
+                    model: Users,
+                    // attributes:['title','content']
                 }]
             });
         }
@@ -26,41 +32,41 @@ export const getDiscussion = async (req , res) => {
 }
 
 export const getDiscussionById = async (req , res) => {
-//     try {
-//         const discussion = Discussion.findOne({
-//             where:{
-//                 uuid: req.params.id
-//             }
-//         });
-//         if(!discussion) return res.status(404).json({msg: "Data tidak ditemukan"});
-//         let response;
-//         if(req.role === "admin"){
-//             response = await Discussion.findOne({
-//                 attributes:['uuid','name','price'],
-//                 where:{
-//                     id: discussion.id
-//                 },
-//                 include:[{
-//                     model: Users,
-//                     attributes:['name','email']
-//                 }]
-//             });
-//         }else{
-//             response = await Discussion.findOne({
-//                 attributes:['uuid','name','price'],
-//                 where:{
-//                     [Op.and]:[{id: discussion.id}, {userId: req.userId}]
-//                 },
-//                 include:[{
-//                     model: Users,
-//                     attributes:['name','email']
-//                 }]
-//             });
-//         }
-//         res.status(200).json(response);
-//     } catch (error) {
-//         res.status(500).json({msg: error.message});
-//     }
+    try {
+        const discussion = await Discussion.findOne({
+            where:{
+                uuid: req.params.id
+            }
+        });
+        if(!discussion) return res.status(404).json({msg: "Data tidak ditemukan"});
+        let response;
+        if(req.role === "admin"){
+            response = await Discussion.findOne({
+                attributes:['uuid','title','content'],
+                where:{
+                    id: discussion.id
+                },
+                include:[{
+                    model: Users,
+                    attributes:['name','email']
+                }]
+            });
+        }else{
+            response = await Discussion.findOne({
+                attributes:['uuid','title','content'],
+                where:{
+                    [Op.and]:[{id: discussion.id}, {userId: req.userId}]
+                },
+                include:[{
+                    model: Users,
+                    attributes:['name','email']
+                }]
+            });
+        }
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 }
     
 
@@ -79,38 +85,59 @@ export const createDiscussion = async (req, res) => {
 }
 
 export const updateDiscussion = async (req , res) => {
-//     try {
-//         const discussion = await Discussion.findOne({
-//             where:{
-//                 uuid: req.params.id
-//             }
-//         });
-//         if(!discussion) return res.status(404).json({msg: "Data tidak ditemukan"});
-//         const {title, content} = req.body;
-//         if(req.role === "admin"){
-//             await Discussion.update({title, content},{
-//                 where:{
-//                     id: discussion.id
-//                 }
-//             });
-//         }else{
-//             if(req.userId !== discussion.userId) return res.status(403).json({msg: "Akses terlarang"});
-//             await Product.update({title, content},{
-//                 where:{
-//                     [Op.and]:[{id: product.id}, {userId: req.userId}]
-//                 }
-//             });
-//         }
-//         res.status(200).json({msg: "Product updated successfuly"});
-//     } catch (error) {
-//         res.status(500).json({msg: error.message});
-//     }
+    try {
+        const discussion = await Discussion.findOne({
+            where:{
+                uuid: req.params.id
+            }
+        });
+        if(!discussion) return res.status(404).json({msg: "Data tidak ditemukan"});
+        const {title, content} = req.body;
+        if(req.role === "admin"){
+            await Discussion.update({title, content},{
+                where:{
+                    id: discussion.id
+                }
+            });
+        }else{
+            if(req.userId !== discussion.userId) return res.status(403).json({msg: "Akses terlarang"});
+            await Discussion.update({title, content},{
+                where:{
+                    [Op.and]:[{id: discussion.id}, {userId: req.userId}]
+                }
+            });
+        }
+        res.status(200).json({msg: "Product updated successfuly"});
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 }
 
 export const deleteDiscussion = async (req , res) => {
     try {
-        
+        const discussion = await Discussion.findOne({
+            where:{
+                uuid: req.params.id
+            }
+        });
+        if(!discussion) return res.status(404).json({msg: "Data tidak ditemukan"});
+        const {title, content} = req.body;
+        if(req.role === "admin"){
+            await Discussion.destroy({
+                where:{
+                    id: product.id
+                }
+            });
+        }else{
+            if(req.userId !== discussion.userId) return res.status(403).json({msg: "Akses terlarang"});
+            await Discussion.destroy({
+                where:{
+                    [Op.and]:[{id: discussion.id}, {userId: req.userId}]
+                }
+            });
+        }
+        res.status(200).json({msg: "Product deleted successfuly"});
     } catch (error) {
-        
+        res.status(500).json({msg: error.message});
     }
 }
